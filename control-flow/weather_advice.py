ee08c4b51e7fa62d010c9eb692eaa748d6fef641
EMIO24 <emmanuel.osarodion@icloud.com>
2024-12-22 13:02:22 +0000

    conditionals

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,12 @@
 #!/bin/python3
+while(1):
 
-weather_condition = input('''What's the weather like today? (sunny/rainy/cold"): ''')
-if weather_condition.lower() == 'sunny':
-    print("Wear a t-shirt and sunglasses.")
-elif weather_condition.lower() == 'rainy':
-    print("Don't forget your umbrella and a raincoat.")
-elif weather_condition.lower() == 'cold':
-    print("Make sure to wear a warm coat and a scarf.")
-else:
-    print("Sorry, I don't have recommendations for this weather.")
+    weather_condition = input('''What's the weather like today? (sunny/rainy/cold"): ''')
+    if weather_condition.lower() == 'sunny':
+        print("Wear a t-shirt and sunglasses.")
+    elif weather_condition.lower() == 'rainy':
+        print("Don't forget your umbrella and a raincoat.")
+    elif weather_condition.lower() == 'cold':
+        print("Make sure to wear a warm coat and a scarf.")
+    else:
+        print("Sorry, I don't have recommendations for this weather.")
