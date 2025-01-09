7a16e50f23f86096934cef8464faa247a84ba132
EMIO24 <emmanuel.osarodion@icloud.com>
2025-01-09 12:28:56 +0100

    module

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,9 +12,9 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            shopping_list.append(input('Enter item: '))
+            shopping_list.append(input("Enter the item to add: "))
         elif choice == '2':
-            shopping_list.remove(input('Enter item: '))
+            shopping_list.remove(input("Enter the item to remove: "))
         elif choice == '3':
             print(shopping_list)
         elif choice == '4':
