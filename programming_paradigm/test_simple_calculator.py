babf385127186f048bd04f7a4990baf21f763c6c
EMIO24 <emmanuel.osarodion@icloud.com>
2025-01-20 13:06:45 +0100

    unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -18,7 +18,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(5, 1), 5)
         
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         
