a723fecc6c6527a6c1130c71a27ca827562e226c
EMIO24 <emmanuel.osarodion@icloud.com>
2025-01-21 14:44:34 +0100

    oop

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out, _books):
+        self.title = title
+        self.author = author
+        self._is_checked_out = True
+        
+class Library(Book):
+    def __init__(self, _books):
+        self._books = []
+        
+    def add_book(self):
+        self._books.append(Book)
+    
+    def check_out_book(self, title):
+        for self.title in self._books:
+            if self.title == title:
+                if self._is_checked_out is False:
+                    self._books.remove(title)
+            else:
+                return 'unavailable'
+    def return_book(self):
+        for title in self._books:
+            if self.title != title:
+                if self._is_checked_out is True:
+                    self._books.append(self)
