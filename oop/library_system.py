6da62758bb4665a1a149a0de7c3caf73275058b9
EMIO24 <emmanuel.osarodion@icloud.com>
2025-01-25 14:10:36 +0100

    oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,25 @@ class Book:
         self.title = title
         self.author = author
         
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}"
+
+        
 class EBook(Book):
     def __init__(self, title, author, file_size):
         Book.__init__(self, title, author)
         self.file_size = file_size
         
+    def __str__(self):
+        return f"{super().__str__()}, File Size: {self.file_size} MB"
+    
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         Book.__init__(self, title, author)
         self.page_count = page_count 
         
+    def __str__(self):
+        return f"{super().__str__()}, Pages: {self.page_count}"        
 class Library:
     def __init__(self, books):
         self.books = books
